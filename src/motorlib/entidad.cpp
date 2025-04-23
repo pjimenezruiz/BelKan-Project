c4bacccb3b0dba7693aa2674c5b6c240eb908afd
Juan Luis <jlsuarezdiaz@ugr.es>
2025-04-23 23:03:15 +0200

    Fixed TURN_SR

--- src/motorlib/entidad.cpp
+++ src/motorlib/entidad.cpp
@@ -360,10 +360,10 @@ int Entidad::fixBateria_sig_accion_jugador(unsigned char celdaJugador, int difAl
     switch (celdaJugador)
     {
     case 'A':
-      bateria_sig_accion = 10;
+      bateria_sig_accion = 16;
       break;
     case 'T':
-      bateria_sig_accion = 5;
+      bateria_sig_accion = 3;
       break;
     } // Fin switch celdaJugador
     break;
